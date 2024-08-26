4a30336a889c0152def773dc58a59946af92cb58
nicole wachter <nicole.wachter6@gmail.com>
2024-08-26 14:35:04 -0400

    secret

--- server/config.py
+++ server/config.py
@@ -1,6 +1,8 @@
 # Standard library imports
 
 # Remote library imports
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from flask_cors import CORS
 from flask_migrate import Migrate
@@ -8,10 +10,14 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
+load_dotenv()
+
+
 # Local imports
 
 # Instantiate app, set attributes
 app = Flask(__name__)
+app.secret_key = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
